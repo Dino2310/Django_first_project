b282e50efcdc448cdb69df25a3d30d09eb9e2bf6
Дмитрий Шмонов <elicta1987@gmail.com>
2024-02-23 10:21:51 +0300

    Добавление шаблонов html и медиа

--- project/app/contentTest.py
+++ project/app/contentTest.py
@@ -4,7 +4,8 @@ conatact = {
     'textOffice':'Расположение нашего офиса',
     'textTel':'Телефон/n(стационарный)',
     'addr':'The interion Design Studio Company The Courtytad, Al, Quoz 1, Колорадо США',
-    'tel':'+ 912 3 567 8987\n+ 912 5 252 3336'
+    'tel':'+ 912 3 567 8987\n+ 912 5 252 3336',
+    'title': "Контакты",
 }
 
 abouts = {'text':'Paragraph. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro repudiandae in debitis error dolorum doloribus perferendis quod eaque inventore aperiam modi ipsam voluptatibus beatae, esse necessitatibus dignissimos distinctio minima et id molestias sed accusantium? Consequatur, a omnis doloremque nesciunt fugiat quo veritatis modi, rerum aliquid distinctio ipsum cupiditate laboriosam at!'
--- project/app/models.py
+++ project/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     def __str__(self):
@@ -13,6 +14,9 @@ class Post(models.Model):
     date_created = models.DateField(auto_now_add = True)
     #DateTimeField  - поле хранящее дату и время
     update_at = models.DateTimeField(auto_now = True)
+    author = models.ForeignKey(User, on_delete = models.CASCADE, null = True)
+    image = models.ImageField(default="image.jpg", blank= True)
+
 
     def slice(self):
         return " ".join(self.content.split()[:3]) + "..."
